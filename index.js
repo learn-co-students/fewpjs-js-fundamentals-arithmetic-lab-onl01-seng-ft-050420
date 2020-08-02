bba863febd8f455db42d981d8021b189da8b3df1
github username <lushiyun@outlook.com>
2020-08-02 17:38:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+
+const { match } = require("sinon");
+
+const num1 = 1;
+const num2 = 62;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = 7 % 3;
+
+function max() {
+  return Math.max(1, 20);
+}
