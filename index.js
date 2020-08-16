e7be341c10a9be3adb94e4eda75629490ef8b8d5
gd-m <gitanjalidmule@gmail.com>
2020-08-16 12:50:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 62;
+let num2 = 1;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = 14 % 5
+
+
+
+const max = Math.max(1,2,5,20)
