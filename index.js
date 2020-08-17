86130c319b1f508030e04e30c3c7f40f8df28ac2
kikuishimoto <kikuish@gmail.com>
2020-08-17 11:04:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.ceil(Math.random()*100)
+const mod = 40 % 6
+const max = Math.max(1, 2, 3, 20)
